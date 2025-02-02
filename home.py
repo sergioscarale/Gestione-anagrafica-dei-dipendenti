9b00962fa03075985144f533561118f097e5ea69
sergioscarale <sergio.scarale11@gmail.com>
2025-02-02 16:38:34 +0100

    gestione delle modifiche fatte
    però ci sono troppi "\n" all'interno del file che quando visualizzo il tutto, nell'output non viene bene, aggiustarlo

--- home.py
+++ home.py
@@ -14,6 +14,7 @@ def inserisciRecord(nomeFile):
             mioFile.write(f"{lunghezzaFile(nomeFile)} {dipendente[0]} {dipendente[1]} {dipendente[2]}\n")
     else:
         with open (nomeFile,"a") as mioFile:
+            mioFile.write(f"{lunghezzaFile(nomeFile)} {dipendente[0]} {dipendente[1]} {dipendente[2]}\n")
             print("file creato!\n")
 
 def visualizzaRecord(nomeFile):
@@ -30,32 +31,101 @@ def gestisciArchivio():
 
 def modTutto(nomeFile):
     with open(nomeFile,"r") as mioFile:
+        nome,cognome=splitNomeCognome(input("\nquale anagrafica vuoi cambiare?\n> "))
+        righe=[]
+        trovato=False
         for linea in mioFile:
-            linea.strip()
+            dati=linea.strip().split(" ")
+            if(nome and cognome in linea):
+                nome,cognome=splitNomeCognome(input("nome e cognome della nuova anagrafica\n> ").lower())
+                stipendio=input("nuovo stipendio\n> ")
+                nuovaRiga=(f"{dati[0]} {nome} {cognome} {stipendio}")
+                righe.append(nuovaRiga+"\n")
+                trovato=True
+            else:
+                righe.append(linea+"\n")
+    if trovato:
+        with open(nomeFile, "w") as mioFile:
+            mioFile.writelines(righe)
+        print("Anagrafica modificata con successo!")
+    else:
+        print("Anagrafica inesistente!")
 
+def modAnagrafica(nomeFile):
+    with open(nomeFile,"r") as mioFile:
+        nome,cognome=splitNomeCognome(input("quale anagrafica vuoi modificare?\n> ").lower())
+        righe=[]
+        trovato=False
+        for linea in mioFile:
+            dati=linea.strip().split(" ")
+            if(nome and cognome in linea):
+                nome,cognome=splitNomeCognome(input("nuova anagrafica\n> "))
+                nuovaRiga=(f"{dati[0]} {nome} {cognome} {dati[3]}")
+                righe.append(nuovaRiga+"\n")
+                trovato=True
+            else:
+                righe.append(linea+"\n")
+    if trovato:
+        with open(nomeFile, "w") as mioFile:
+            mioFile.writelines(righe)
+        print("Anagrafica modificata con successo!")
+    else:
+        print("Anagrafica inesistente!")
 
-#def modAnagrafica():
-
-
-#def modStipendio():
-
+def modStipendio(nomeFile):
+    with open(nomeFile,"r") as mioFile:
+        trovato=False
+        righe=[]
+        nome,cognome=splitNomeCognome(input("stipendio dell'anagrafica da modificare\n> ").lower())
+        for linea in mioFile:
+            dati=linea.strip().split(" ")
+            if(nome and cognome in linea):
+                stipendio=input("nuovo stipendio\n> ")
+                nuovaRiga=(f"{dati[0]} {nome} {cognome} {stipendio}")
+                righe.append(nuovaRiga+"\n")
+                trovato=True
+            else:
+                righe.append(linea+"\n")
+    if trovato:
+        with open(nomeFile,"w") as mioFile:
+            mioFile.writelines(righe)
+        print("Stipendio modificato con successo!")
+    else:
+        print("Anagrafica inesistente!")
 
 def modificaRecord(nomeFile):
     scelta="1"
     if(os.path.exists(nomeFile)):
             while scelta!="0":
-                print("quale campo vuoi modificare?\n0-Esci\n1-Anagrafica e stipendio\n2-Anagrafica\n3-Stipendio")
+                print("\nquale campo vuoi modificare?\n0-Esci\n1-Anagrafica e stipendio\n2-Anagrafica\n3-Stipendio")
                 scelta=input("\n> ")
                 match scelta:
                     case "1": modTutto(nomeFile)
-                    #case "2": modAnagrafica()
-                    #case "3": modStipendio()
+                    case "2": modAnagrafica(nomeFile)
+                    case "3": modStipendio(nomeFile)
     else:
         with open (nomeFile,"a") as mioFile:
             print("file creato!\n")
 
-def eliminaRecord():
-    print("ciao")
+def eliminaRecord(nomeFile):
+    with open(nomeFile,"r") as mioFile:
+        trovato=False
+        righe=[]
+        nome,cognome=splitNomeCognome(input("anagrafica da cancellare\n> ").lower())
+        for linea in mioFile:
+            dati=linea.strip().split(" ")
+            if(nome and cognome in linea):
+                trovato=True
+                nuovaRiga=("")
+                righe.append(nuovaRiga+"\n")
+            else:
+                righe.append(linea+"\n")
+    if trovato:
+        with open(nomeFile,"w") as mioFile:
+            mioFile.writelines(righe) 
+            print("record cancellato!")
+    else:
+        print("record inesistente!")
 
 def main():
     nomeFile="anagrafe.txt"
@@ -73,6 +143,6 @@ def main():
             case 2: visualizzaRecord(nomeFile)
             case 3: gestisciArchivio()
             case 4: modificaRecord(nomeFile)
-            case 5: eliminaRecord()
+            case 5: eliminaRecord(nomeFile)
 
 main()
